914a67d948f1ef9a097e07c30945fa616123eaf5
Fleed1x22 <zaharnelubovskij@gmail.com>
2026-04-08 19:47:08 +0300

    Add files via upload

--- Передача статического массива в качестве параметра функции.cpp
+++ Передача статического массива в качестве параметра функции.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 using namespace std;
-
-/* в функцию в качестве параметров передаются статический массив mas 
-и его размерность n */
+/* в функцию в качестве параметров передаются статический массив таза и его размерность n */
 void print (int mas[], int n)
 {
     for (int i = 0; i < n; i++) cout << mas[i] << "\t";
-    cout << endl;
+    cout<< endl;
 }
 
 int main()
@@ -14,7 +12,7 @@ int main()
     int a[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     print(a, 10);
     cout << endl;
-    print(a, 7);
+    print (a, 7);
     print(a, 13);
     return 0;
 }
